f53eb38b59007200780e1bfa86bb56b6b1519633
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-12-18 05:53:51 +1000

    Init firebaserc and add firebase auth service

---
+++ checkout-client/src/service.js
@@ -0,0 +1,26 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcSDSnXW1itPWY8lE20Nmj8ltVheprWik",
+  authDomain: "fir-checkout-challenge.firebaseapp.com",
+  databaseURL: "https://fir-checkout-challenge.firebaseio.com",
+  projectId: "fir-checkout-challenge"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+const db = firebase.firestore();
+
+// update firestore settings
+db.settings({ timestampsInSnapshots: true });
+
+
