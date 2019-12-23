82b94cb4d2993344fe6def6a0a9eaaa889cb75d3
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-12-23 14:34:02 +1100

    Clean up debug print

--- checkout-client/src/api.js
+++ checkout-client/src/api.js
@@ -1,20 +1,25 @@
 import axios from "axios";
 
 export async function getApi() {
-  
   let api = await axios.get(
     "https://us-central1-fir-checkout-challenge.cloudfunctions.net/app/api"
   );
-  console.log(api.data);
   return api.data;
 }
 
-export async function admin_only(email) {
-  let res = await axios.post(
-    "https://us-central1-fir-checkout-challenge.cloudfunctions.net/app/api/admin_only",
-    email
+export async function admin_only(user) {
+  let token = await user.getIdToken()
+  return axios.get(
+    "http://us-central1-fir-checkout-challenge.cloudfunctions.net/app/api/admin_only",
+    { headers: { Authorization: `Bearer ${token}` } }
   );
-  
-  console.log(res.data, '@@@');
-  return res.data;
+}
+
+export function addAdmin(email) {
+  return axios.post(
+    "https://us-central1-fir-checkout-challenge.cloudfunctions.net/app/api/admin_only",
+    {
+      email: email
+    }
+  );
 }
